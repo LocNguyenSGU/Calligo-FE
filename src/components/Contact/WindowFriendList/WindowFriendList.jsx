1b1a6b89aa4d1e2222bdecbeea93fbcbd5275019
LocNguyenSGU <lockbkbang@gmail.com>
2025-02-04 16:22:45 +0700

    style: layout list item friend request

--- src/components/Contact/WindowFriendList/WindowFriendList.jsx
+++ src/components/Contact/WindowFriendList/WindowFriendList.jsx
@@ -12,7 +12,7 @@ const WindowFriendList = () => {
     const [error, setError] = useState(null); // Lưu lỗi nếu có
     const [name, setName] = useState("");
     const nameDebounce = useDebounce(name, 500);
-    const [sort, setSort] = useState("Increase"); // Mặc định là tăng dần
+    const [sort, setSort] = useState("Increase");
 
     useEffect(() => {
         const fetchFriends = async () => {
@@ -27,9 +27,9 @@ const WindowFriendList = () => {
                 }
 
                 // Sắp xếp danh sách theo thứ tự lastName
-                let sortedFriends = response.data.content.sort((a, b) => 
-                    sort === "Increase" 
-                        ? a.lastName.localeCompare(b.lastName) 
+                let sortedFriends = response.data.content.sort((a, b) =>
+                    sort === "Increase"
+                        ? a.lastName.localeCompare(b.lastName)
                         : b.lastName.localeCompare(a.lastName)
                 );
 
@@ -66,10 +66,10 @@ const WindowFriendList = () => {
             <div className="body bg-white h-screen w-100% rounded-md ml-4">
                 <div className="header-action flex gap-3 p-3">
                     <div className="w-[80%]">
-                        <Input 
-                            placeholder="Tìm kiếm" 
-                            prefix={<SearchOutlined />} 
-                            onChange={(e) => setName(e.target.value)} 
+                        <Input
+                            placeholder="Tìm kiếm"
+                            prefix={<SearchOutlined />}
+                            onChange={(e) => setName(e.target.value)}
                         />
                     </div>
                     <div className="w-[20%]">
@@ -86,35 +86,35 @@ const WindowFriendList = () => {
                 </div>
                 <div className="body-list mt-2">
                     {loading ? (
-                       <>
-                       <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
-                           <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
-                               <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
-                           </div>
-                           <span className="font-semibold text-base">
-                               <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
-                           </span>
-                       </div>
-                       <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
-                       <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
-                           <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
-                               <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
-                           </div>
-                           <span className="font-semibold text-base">
-                               <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
-                           </span>
-                       </div>
-                       <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
-                       <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
-                           <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
-                               <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
-                           </div>
-                           <span className="font-semibold text-base">
-                               <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
-                           </span>
-                       </div>
-                       <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
-                   </>
+                        <>
+                            <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                                <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                    <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                                </div>
+                                <span className="font-semibold text-base">
+                                    <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                                </span>
+                            </div>
+                            <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                            <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                                <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                    <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                                </div>
+                                <span className="font-semibold text-base">
+                                    <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                                </span>
+                            </div>
+                            <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                            <div className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                                <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                    <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                                </div>
+                                <span className="font-semibold text-base">
+                                    <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                                </span>
+                            </div>
+                            <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                        </>
                     ) : (
                         Object.keys(grouped).length === 0 ? (
                             <span>Không có bạn bè</span>
@@ -123,12 +123,15 @@ const WindowFriendList = () => {
                                 <div key={letter} className="block">
                                     <h2 className="font-semibold text-base pl-3">{letter}</h2>
                                     {grouped[letter].map(friend => (
-                                        <div key={friend.id} className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
-                                            <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
-                                                <img src="/public/sidebar/boy.png" alt="" className="w-12 h-12 rounded-full" />
+                                        <>
+                                            <div key={friend.id} className="item flex gap-3 items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                                                <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                                    <img src="/public/sidebar/boy.png" alt="" className="w-12 h-12 rounded-full" />
+                                                </div>
+                                                <span className="font-semibold text-base">{friend.firstName} {friend.lastName}</span>
                                             </div>
-                                            <span className="font-semibold text-base">{friend.firstName} {friend.lastName}</span>
-                                        </div>
+                                            <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                                        </>
                                     ))}
                                 </div>
                             ))
--- src/components/Contact/WindowFriendRequest/WindowFriendRequest.jsx
+++ src/components/Contact/WindowFriendRequest/WindowFriendRequest.jsx
@@ -1,18 +1,131 @@
+import { SearchOutlined } from "@ant-design/icons";
+import { Button, Input, Select } from "antd";
+import { useState } from "react";
+import LoadingSkeleton from "../../shared/LoadingSkeleton";
 
 const WindowFriendRequest = () => {
+    const [sort, setSort] = useState("Increase");
+    const [name, setName] = useState("");
+    const [loading, setLoading] = useState(true)
+    const [friendRequest, setFriendRequest] = useState([])
     return (
         <div className='bg-gray-200'>
-        <div className="header">
-            <div className="flex items-center gap-3 p-5 bg-white border border-solid border-gray-200 border-t-0">
-                <img src="/public/contact/friend_request.png" className="w-8 h-8 p-1" alt="Friend Request" />
-                <span className="block text-base font-medium">Lời mời kết bạn</span>
+            <div className="header">
+                <div className="flex items-center gap-3 p-5 bg-white border border-solid border-gray-200 border-t-0">
+                    <img src="/public/contact/friend_request.png" className="w-8 h-8 p-1" alt="Friend Request" />
+                    <span className="block text-base font-medium">Lời mời kết bạn</span>
+                </div>
+            </div>
+            <span className="font-normal text-sm p-4 block">Lời mời kết bạn nhân được (64)</span>
+
+            <div className="body bg-green-100 h-screen w-100% rounded-md ml-4">
+                <div className="header-action flex gap-3 p-3">
+                    <div className="w-[80%]">
+                        <Input
+                            placeholder="Tìm kiếm"
+                            prefix={<SearchOutlined />}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </div>
+                    <div className="w-[20%]">
+                        <Select
+                            value={sort}
+                            style={{ width: 200 }}
+                            onChange={setSort}
+                            options={[
+                                { value: 'Increase', label: 'Tăng dần (A-Z)' },
+                                { value: 'Descrease', label: 'Giảm dần (Z-A)' }
+                            ]}
+                        />
+                    </div>
+                </div>
+                {loading && (<>
+                    <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                        <div className="flex items-center gap-3">
+                            <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                            </div>
+                            <span className="font-semibold text-base">
+                                <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                            </span>
+                        </div>
+                        <div className="flex gap-3 mt-3 pr-16 h-[32px] w-[256px]">
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                        </div>
+                    </div>
+                    <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                    <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                        <div className="flex items-center gap-3">
+                            <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                            </div>
+                            <span className="font-semibold text-base">
+                                <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                            </span>
+                        </div>
+                        <div className="flex gap-3 mt-3 pr-16 h-[32px] w-[256px]">
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                        </div>
+                    </div>
+                    <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                    <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                        <div className="flex items-center gap-3">
+                            <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                                <LoadingSkeleton width="100%" height="100%" radius="50%"></LoadingSkeleton>
+                            </div>
+                            <span className="font-semibold text-base">
+                                <LoadingSkeleton width="300px" height="15px" radius="4px"></LoadingSkeleton>
+                            </span>
+                        </div>
+                        <div className="flex gap-3 mt-3 pr-16 h-[32px] w-[256px]">
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                            <LoadingSkeleton width="100%" height="100%" radius="8px"></LoadingSkeleton>
+                        </div>
+                    </div>
+                </>)}
+                <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                    <div className="flex items-center gap-3">
+                        <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                            <img src="/public/sidebar/boy.png" alt="" className="w-12 h-12 rounded-full" />
+                        </div>
+                        <span className="font-semibold text-base">Tran ban a</span>
+                    </div>
+                    <div className="flex gap-3 mt-3 pr-16">
+                        <Button>Từ chối</Button>
+                        <Button type="primary">Chấp nhận</Button>
+                    </div>
+                </div>
+                <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                    <div className="flex items-center gap-3">
+                        <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                            <img src="/public/sidebar/boy.png" alt="" className="w-12 h-12 rounded-full" />
+                        </div>
+                        <span className="font-semibold text-base">Tran ban a</span>
+                    </div>
+                    <div className="flex gap-3 mt-3 pr-16">
+                        <Button>Từ chối</Button>
+                        <Button type="primary">Chấp nhận</Button>
+                    </div>
+                </div>
+                <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
+                <div className="item flex justify-between items-center py-3 hover:bg-gray-100 pl-3 cursor-pointer">
+                    <div className="flex items-center gap-3">
+                        <div className="w-[50px] h-[50px] rounded-full border border-gray-400">
+                            <img src="/public/sidebar/boy.png" alt="" className="w-12 h-12 rounded-full" />
+                        </div>
+                        <span className="font-semibold text-base">Tran ban a</span>
+                    </div>
+                    <div className="flex gap-3 mt-3 pr-16">
+                        <Button>Từ chối</Button>
+                        <Button type="primary">Chấp nhận</Button>
+                    </div>
+                </div>
+                <div className="h-[1px] bg-gray-300 w-[90%] my-0 mx-auto"></div>
             </div>
         </div>
-        <span className="font-normal text-sm p-4 block">Lời mời kết bạn nhân được (64)</span>
-        <div className="body bg-green-100 h-screen w-100% rounded-md ml-4">
-
-        </div>
-    </div>
     );
 };
 
